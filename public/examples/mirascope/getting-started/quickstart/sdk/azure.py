37e91d3ed5f157fdcf812e7d89b023ad4a140a59
Dandelion Mané <dandelion@mirascope.com>
2025-04-01 18:04:58 -0700

    chore: swap quickstart examples back to response model

--- public/examples/mirascope/getting-started/quickstart/sdk/azure.py
+++ public/examples/mirascope/getting-started/quickstart/sdk/azure.py
@@ -1,22 +1,51 @@
 from azure.ai.inference import ChatCompletionsClient
-from azure.ai.inference.models import ChatRequestMessage
+from azure.ai.inference.models import (
+    ChatCompletionsToolDefinition,
+    ChatRequestMessage,
+    FunctionDefinition,
+)
 from azure.core.credentials import AzureKeyCredential
+from pydantic import BaseModel
 
 client = ChatCompletionsClient(
     endpoint="YOUR_ENDPOINT", credential=AzureKeyCredential("YOUR_KEY")
 )
 
 
-def recommend_book(genre: str) -> str:
+class Book(BaseModel):
+    """An extracted book."""
+
+    title: str
+    author: str
+
+
+def extract_book(text: str) -> Book:
     completion = client.complete(
         model="gpt-4o-mini",
-        messages=[
-            ChatRequestMessage({"role": "user", "content": f"Recommend a {genre} book"})
+        messages=[ChatRequestMessage({"role": "user", "content": f"Extract {text}"})],
+        tools=[
+            ChatCompletionsToolDefinition(
+                function=FunctionDefinition(
+                    name="Book",
+                    description="An extracted book.",
+                    parameters={
+                        "properties": {
+                            "title": {"type": "string"},
+                            "author": {"type": "string"},
+                        },
+                        "required": ["title", "author"],
+                        "type": "object",
+                    },
+                )
+            )
         ],
+        tool_choices="required",
     )
-    message = completion.choices[0].message
-    return message.content if message.content is not None else ""
+    if tool_calls := completion.choices[0].message.tool_calls:
+        return Book.model_validate_json(tool_calls[0].function.arguments)
+    raise ValueError("No tool call found")
 
 
-output = recommend_book("fantasy")
-print(output)
+book = extract_book("The Name of the Wind by Patrick Rothfuss")
+print(book)
+# Output: title='The Name of the Wind' author='Patrick Rothfuss'
--- public/examples/mirascope/getting-started/quickstart/sdk/cohere.py
+++ public/examples/mirascope/getting-started/quickstart/sdk/cohere.py
@@ -1,15 +1,41 @@
 from cohere import Client
+from cohere.types import Tool, ToolParameterDefinitionsValue
+from pydantic import BaseModel
 
 client = Client()
 
 
-def recommend_book(genre: str) -> str:
+class Book(BaseModel):
+    """An extracted book."""
+
+    title: str
+    author: str
+
+
+def extract_book(text: str) -> Book:
     response = client.chat(
         model="command-r-plus",
-        message=f"Recommend a {genre} book",
+        message=f"Extract {text}",
+        tools=[
+            Tool(
+                name="Book",
+                description="An extracted book.",
+                parameter_definitions={
+                    "title": ToolParameterDefinitionsValue(
+                        description=None, type="string", required=True
+                    ),
+                    "author": ToolParameterDefinitionsValue(
+                        description=None, type="string", required=True
+                    ),
+                },
+            )
+        ],
     )
-    return response.text
+    if response.tool_calls:
+        return Book.model_validate(response.tool_calls[0].parameters)
+    raise ValueError("No tool call found")
 
 
-output = recommend_book("fantasy")
-print(output)
+book = extract_book("The Name of the Wind by Patrick Rothfuss")
+print(book)
+# Output: title='The Name of the Wind' author='Patrick Rothfuss'
