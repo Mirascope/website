402ae2fc819311c6311e52ca50f46320b6843fa4
Dandelion Mané <dandelion@mirascope.com>
2025-04-08 14:18:09 -0700

    feat: add GitHub stats in header with contextual repo info (#25)

--- src/lib/constants/site.ts
+++ src/lib/constants/site.ts
@@ -2,8 +2,35 @@
  * Site-wide constants
  */
 
+// Import GitHub stats from build-time generated file
+import githubStats from "./github-stats.json";
+
 // Site version information
 export const SITE_VERSION = "1.1";
 
 // Analytics constants
 export const GA_MEASUREMENT_ID = "G-DJHT1QG9GK";
+
+// Product configurations
+export const PRODUCT_CONFIGS = {
+  mirascope: {
+    title: "Mirascope",
+    color: "#6366f1",
+    description: "LLM abstractions that aren't obstructions.",
+    github: {
+      repo: "Mirascope/mirascope",
+      stars: githubStats.mirascope.stars,
+      version: githubStats.mirascope.version,
+    },
+  },
+  lilypad: {
+    title: "Lilypad",
+    color: "#2d8031",
+    description: "Start building your data flywheel in one line of code.",
+    github: {
+      repo: "Mirascope/lilypad",
+      stars: githubStats.lilypad.stars,
+      version: githubStats.lilypad.version,
+    },
+  },
+};
