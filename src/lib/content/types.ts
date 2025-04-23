5f4450bde33fe668c53e4da170c2995b272e346d
Dandelion Mané <dandelion@dandelion.art>
2025-04-23 16:41:03 -0700

    chore: remove vestigal code

--- src/lib/content/types.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Re-export all types from content-types.ts for backward compatibility
- * @deprecated Please import directly from content-types.ts instead
- */
-
-export * from "./content-types";
