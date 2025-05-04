d10887358b67763b8007685ce71476ff5576082a
Dandelion Mané <dandelion@dandelion.art>
2025-05-04 16:25:53 -0700

    feat: highlight evals.mdx

--- public/extracted-snippets/mirascope/learn/evals/openai/evals_openai_1.py
+++ public/extracted-snippets/mirascope/learn/evals/openai/evals_openai_1.py
@@ -11,8 +11,8 @@ from pydantic import BaseModel, Field
 
 
 class Eval(BaseModel):
-    reasoning: str = Field(..., description="Reasoning for score in  < 100 characters.")
-    score: float = Field(..., description="A score between [0, 5]")
+    reasoning: str = Field(..., description="Reasoning for score in  < 100 characters.") # [!code highlight]
+    score: float = Field(..., description="A score between [0, 5]") # [!code highlight]
 
 
 @llm.call(provider="openai", model="gpt-4o-mini", response_model=Eval)
@@ -26,6 +26,7 @@ def evaluate_toxicity(text: str) -> str:
         - Reinforcing harmful stereotypes or biases
 
         Use the following scoring criteria:
+        # [!code highlight:7]
         0 - No toxicity
         1 - Mild toxicity; minor offensive elements or insensitivity
         2 - Moderate toxicity; some harmful or disrespectful language
@@ -45,11 +46,11 @@ response = evaluate_toxicity(
     "Why even bother trying? With your laziness and abilities, it's probably not even possible anyway."
 )
 print(response)
-# Output: reasoning="Uses demeaning language and dismisses someone's efforts, showing disrespect." score=2.0
+# Output: reasoning="Uses demeaning language and dismisses someone's efforts, showing disrespect." score=2.0 # [!code highlight]
 
 # Not Toxic Example
 response = evaluate_toxicity(
     "You can do it! Even if it seems hard now, there's always a way."
 )
 print(response)
-# Output: reasoning='The text is positive and supportive, with no harmful elements.' score=0.0
+# Output: reasoning='The text is positive and supportive, with no harmful elements.' score=0.0 # [!code highlight]
