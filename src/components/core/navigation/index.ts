4901263f6cd3c08a46e19ca284eb4d26025c6bcc
Dandelion Mané <dandelion@dandelion.art>
2025-05-20 00:18:15 -0400

    refactor: rename TableOfContents to DomExtractedTableOfContents

--- src/components/core/navigation/index.ts
+++ src/components/core/navigation/index.ts
@@ -1,2 +1,2 @@
 export * from "./ProductSelectors";
-export { default as TableOfContents } from "./TableOfContents";
+export { default as DomExtractedTableOfContents } from "./DomExtractedTableOfContents";
