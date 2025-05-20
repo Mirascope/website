6730b73bed9e29db9d77ea6308d50da0b4ce14fa
Dandelion Mané <dandelion@dandelion.art>
2025-05-20 00:22:02 -0400

    refactor: extract TableOfContents from DomExtractedTableOfContents

--- src/components/core/navigation/index.ts
+++ src/components/core/navigation/index.ts
@@ -1,2 +1,3 @@
 export * from "./ProductSelectors";
 export { default as DomExtractedTableOfContents } from "./DomExtractedTableOfContents";
+export { TableOfContents, type TOCItem } from "./TableOfContents";
