744c9205b6a0f02b5988011a2633113badd422d6
William Bakst <william@mirascope.io>
2025-06-30 19:04:27 -0700

    fix: issue with og meta build since router waitlist had no title

--- src/components/routes/router-waitlist/RouterWaitlist.tsx
+++ src/components/routes/router-waitlist/RouterWaitlist.tsx
@@ -1,16 +1,20 @@
+import { PageMeta } from "@/src/components/";
 import { RouterWaitlistForm } from "./RouterWaitlistForm";
 import { MirascopeV2Block } from "./MirascopeV2Block";
 import { ProviderContextProvider } from "@/src/components/core/providers/ProviderContext";
 
 export function RouterWaitlist() {
   return (
-    <div className="mt-32">
-      <ProviderContextProvider>
-        <div data-gradient-fade={true} className="mb-4">
-          <MirascopeV2Block />
-        </div>
-      </ProviderContextProvider>
-      <RouterWaitlistForm />
-    </div>
+    <>
+      <PageMeta title="Router Waitlist" description="The AI Engineer's Developer Stack" />
+      <div className="mt-32">
+        <ProviderContextProvider>
+          <div data-gradient-fade={true} className="mb-4">
+            <MirascopeV2Block />
+          </div>
+        </ProviderContextProvider>
+        <RouterWaitlistForm />
+      </div>
+    </>
   );
 }
