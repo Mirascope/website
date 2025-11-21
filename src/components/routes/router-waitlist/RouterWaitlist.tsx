857c032fa7923581a24b48622efafcb486ccd2be
Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>
2025-11-21 10:45:26 -0800

    Move RunnableProvider up component tree for DRY
    Signed-off-by: Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>

--- src/components/routes/router-waitlist/RouterWaitlist.tsx
+++ src/components/routes/router-waitlist/RouterWaitlist.tsx
@@ -1,7 +1,7 @@
 import { PageMeta } from "@/src/components/";
 import { RouterWaitlistForm } from "./RouterWaitlistForm";
 import { MirascopeV2Block } from "./MirascopeV2Block";
-import { ProviderContextProvider, RunnableProvider } from "@/src/components/core";
+import { ProviderContextProvider } from "@/src/components/core";
 
 export function RouterWaitlist() {
   return (
@@ -9,11 +9,9 @@ export function RouterWaitlist() {
       <PageMeta title="Router Waitlist" description="The AI Engineer's Developer Stack" />
       <div className="mt-4 sm:mt-32">
         <ProviderContextProvider>
-          <RunnableProvider>
-            <div data-gradient-fade={true} className="mb-4">
-              <MirascopeV2Block />
-            </div>
-          </RunnableProvider>
+          <div data-gradient-fade={true} className="mb-4">
+            <MirascopeV2Block />
+          </div>
         </ProviderContextProvider>
         <RouterWaitlistForm />
       </div>
