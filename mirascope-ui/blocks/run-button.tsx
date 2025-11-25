2b5c0cb85f635d5b2e3eadc816904a251a3da770
Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>
2025-11-25 14:49:00 -0800

    Remove cruft
    Signed-off-by: Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>

--- mirascope-ui/blocks/run-button.tsx
+++ mirascope-ui/blocks/run-button.tsx
@@ -5,27 +5,6 @@ export interface RunButtonProps {
 }
 
 export const RunButton = ({ onRun }: RunButtonProps) => {
-  // const runnable = useRunnable();
-  // const [running, setRunning] = useState(false);
-  // const runCode = useCallback(() => {
-  //   if (!runnable || !runnable.stdout || !runnable.stderr) {
-  //     return;
-  //   }
-
-  //   console.log("running code");
-  //   setRunning(true);
-  //   onRun?.();
-
-  //   // avoid blocking the main thread
-  //   setTimeout(() => {
-  //     runnable.runPython(content).then(() => {
-  //       console.log("done running code");
-  //       onRun?.();
-  //       setRunning(false);
-  //     });
-  //   }, 50);
-  // }, [content, runnable, runnable.stdout, runnable.stderr, onRun]);
-
   return (
     <button
       className="bg-background hover:bg-muted relative cursor-pointer rounded-md border p-1.5"
@@ -33,7 +12,6 @@ export const RunButton = ({ onRun }: RunButtonProps) => {
       title="Run code"
       onClick={onRun}
     >
-      {/* {running ? <LoaderCircle className="size-4 animate-spin" /> : <Play className="size-4" />} */}
       <Play className="size-4" />
     </button>
   );
